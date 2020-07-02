79f0dc847458fa37c6d0384cf9e869f8e62df52b
shadyhosam200 <32609128+shadyhosam200@users.noreply.github.com>
2020-07-02 18:12:26 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int counter=0 ;
+
+    string name;
+    cin >> name;
+
+    sort(name.begin(), name.end());
+
+    string temp="";
+    temp = name;
+
+    for (int i = 0; i <temp.length()  ; i++)
+    {
+
+
+        if (name[i]==name[i+1])
+            counter++;
+
+
+
+    }
+
+    int final = temp.length() - counter;
+    if (final%2==0)
+    {
+        cout <<"CHAT WITH HER!" << endl;
+    }
+    else
+        cout <<"IGNORE HIM!" << endl;
+}
